bb1e63e48b7532dc07011f8ea22b62f6c1b92436
Taylor Geisse <1383010+tgeisse@users.noreply.github.com>
2024-03-27 09:57:10 -0700

    Initial Commit

---
+++ UserDefaultsObservation Examples/UserDefaultsObservation_ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsObservation_ExamplesApp.swift
+//  UserDefaultsObservation Examples
+//
+//  Created by Taylor Geisse on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsObservation_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
